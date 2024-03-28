8aac9d44be5fb4ba312ccc7d5dbef4a199417f75
danfio7 <daniele.fiorucci@iisgubbio.edu.it>
2024-03-28 15:45:49 +0100

    aggiunto terreno e dinosauro

--- src/it/edu/iisgubbio/dino/ProgrammaPrincipale.java
+++ src/it/edu/iisgubbio/dino/ProgrammaPrincipale.java
@@ -25,21 +25,34 @@ public class ProgrammaPrincipale extends Application{
     ImageView montagneView = new ImageView(montagne);
     Image montagne2 = new Image(getClass().getResourceAsStream("Sfondoprogramma_montagne.png"));
     ImageView montagneView2 = new ImageView(montagne2);
+    Image terreno = new Image(getClass().getResourceAsStream("Sfondoprogramma_terreno.PNG"));
+    ImageView terrenoView = new ImageView(terreno);
+    Image terreno2 = new Image(getClass().getResourceAsStream("Sfondoprogramma_terreno.PNG"));
+    ImageView terrenoView2 = new ImageView(terreno2);
+    Image dinosauro = new Image(getClass().getResourceAsStream("dinosauroProgramma.PNG"));
+    ImageView dinosauroView = new ImageView(dinosauro);
 	public void start (Stage finestra) {
 		Pane quadro= new Pane();
 		
 		timelineSfondo.setCycleCount(Timeline.INDEFINITE);
 		timelineSfondo.play();
-	    
-	    
-	    
+	   
 	    montagneView.setY(160);
 	    montagneView2.setY(160);
+	    
+	    terrenoView.setY(370);
+	    terrenoView2.setY(370);
+	    
+	    dinosauroView.setX(50);
+	    dinosauroView.setY(300);
 	    
 	    quadro.getChildren().add(cieloView);
 	    quadro.getChildren().add(cieloView2);
 	    quadro.getChildren().add(montagneView);
 	    quadro.getChildren().add(montagneView2);
+	    quadro.getChildren().add(terrenoView);
+	    quadro.getChildren().add(terrenoView2);
+	    quadro.getChildren().add(dinosauroView);
 	    
 	    Scene scena = new Scene (quadro, 750, 500);
 		finestra.setTitle("Dino");
@@ -52,23 +65,35 @@ public class ProgrammaPrincipale extends Application{
 		cieloView2.setX(800);
 		montagneView.setFitWidth(800);
 		montagneView2.setX(800);
+		terrenoView.setFitWidth(800);
+		terrenoView2.setX(800);
+		dinosauroView.setFitHeight(100);
+		dinosauroView.setPreserveRatio(true);
 	}
 	private void aggiornaSfondo() {
-		cieloView.setX(cieloView.getX()-0.2);
-		cieloView2.setX(cieloView2.getX()-0.2);
+		cieloView.setX(cieloView.getX()-0.25);
+		cieloView2.setX(cieloView2.getX()-0.25);
 		if(cieloView2.getX()==0) {
-			cieloView.setX(720);
+			cieloView.setX(735);
 		}
 		if(cieloView.getX()==0) {
 			cieloView2.setX(800);
 		}
-		montagneView.setX(montagneView.getX()-0.3);
-		montagneView2.setX(montagneView2.getX()-0.3);
+		montagneView.setX(montagneView.getX()-0.5);
+		montagneView2.setX(montagneView2.getX()-0.5);
 		if(montagneView2.getX()==0) {
 			montagneView.setX(720);
 		}
 		if(montagneView.getX()==0) {
 			montagneView2.setX(800);
+		}
+		terrenoView.setX(terrenoView.getX()-1);
+		terrenoView2.setX(terrenoView2.getX()-1);
+		if(terrenoView2.getX()==0) {
+			terrenoView.setX(720);
+		}
+		if(terrenoView.getX()==0) {
+			terrenoView2.setX(800);
 		}
 	}
 	public static void main(String[] args) {
