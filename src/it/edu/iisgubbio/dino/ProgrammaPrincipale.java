d032c8c68417e2f3c14e99fd65c347d8ad052f99
danfio7 <daniele.fiorucci@iisgubbio.edu.it>
2024-03-30 16:15:21 +0100

    salto del dino

--- src/it/edu/iisgubbio/dino/ProgrammaPrincipale.java
+++ src/it/edu/iisgubbio/dino/ProgrammaPrincipale.java
@@ -9,14 +9,19 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class ProgrammaPrincipale extends Application{
+	int velocitàDino=20;
 	Timeline timelineSfondo = new Timeline(new KeyFrame(
 			Duration.millis(8),
 			x -> aggiornaSfondo()));
+	Timeline timelineMovDino = new Timeline(new KeyFrame(
+			Duration.millis(velocitàDino),
+			x -> muoviDino()));
 	Image cielo = new Image(getClass().getResourceAsStream("Sfondoprogramma_cielo.jpeg"));
     ImageView cieloView = new ImageView(cielo);
     Image cielo2 = new Image(getClass().getResourceAsStream("Sfondoprogramma_cielo.jpeg"));
@@ -31,11 +36,25 @@ public class ProgrammaPrincipale extends Application{
     ImageView terrenoView2 = new ImageView(terreno2);
     Image dinosauro = new Image(getClass().getResourceAsStream("dinosauroProgramma.PNG"));
     ImageView dinosauroView = new ImageView(dinosauro);
+    boolean arrivatoSù=false;
+    boolean arrivatoGiù=false;
+    
 	public void start (Stage finestra) {
 		Pane quadro= new Pane();
 		
+		quadro.getChildren().add(cieloView);
+	    quadro.getChildren().add(cieloView2);
+	    quadro.getChildren().add(montagneView);
+	    quadro.getChildren().add(montagneView2);
+	    quadro.getChildren().add(terrenoView);
+	    quadro.getChildren().add(terrenoView2);
+	    quadro.getChildren().add(dinosauroView);
+	    
 		timelineSfondo.setCycleCount(Timeline.INDEFINITE);
 		timelineSfondo.play();
+		
+		timelineMovDino.setCycleCount(Timeline.INDEFINITE);
+		
 	   
 	    montagneView.setY(160);
 	    montagneView2.setY(160);
@@ -46,20 +65,13 @@ public class ProgrammaPrincipale extends Application{
 	    dinosauroView.setX(50);
 	    dinosauroView.setY(300);
 	    
-	    quadro.getChildren().add(cieloView);
-	    quadro.getChildren().add(cieloView2);
-	    quadro.getChildren().add(montagneView);
-	    quadro.getChildren().add(montagneView2);
-	    quadro.getChildren().add(terrenoView);
-	    quadro.getChildren().add(terrenoView2);
-	    quadro.getChildren().add(dinosauroView);
-	    
 	    Scene scena = new Scene (quadro, 750, 500);
+	    scena.getStylesheets().add("it/edu/iisgubbio/dino/Dino.css");
+	    scena.setOnKeyPressed(e -> pigiato(e));
+	    
 		finestra.setTitle("Dino");
 		finestra.setScene(scena);
 		finestra.show();
-		scena.getStylesheets().add("it/edu/iisgubbio/dino/Dino.css");
-		cieloView.setId("pannello");
 		
 		cieloView.setFitWidth(800);
 		cieloView2.setX(800);
@@ -87,13 +99,41 @@ public class ProgrammaPrincipale extends Application{
 		if(montagneView.getX()==0) {
 			montagneView2.setX(800);
 		}
-		terrenoView.setX(terrenoView.getX()-1);
-		terrenoView2.setX(terrenoView2.getX()-1);
+		terrenoView.setX(terrenoView.getX()-5);
+		terrenoView2.setX(terrenoView2.getX()-5);
 		if(terrenoView2.getX()==0) {
 			terrenoView.setX(720);
 		}
 		if(terrenoView.getX()==0) {
 			terrenoView2.setX(800);
+		}
+	}
+	private void muoviDino() {
+		if(dinosauroView.getY()>=200 && arrivatoSù==false) {
+			dinosauroView.setY(dinosauroView.getY()-10);
+			System.out.println("1");
+			if(dinosauroView.getY()==200) {
+				System.out.println("arrivato!");
+				arrivatoSù=true;
+			}
+		}else {
+			if(dinosauroView.getY()<=300 && arrivatoSù) {
+				dinosauroView.setY(dinosauroView.getY()+10);
+				System.out.println("2");
+				if(dinosauroView.getY()==300) {
+					arrivatoGiù=true;
+				}
+			}
+		}
+		if(arrivatoGiù==true) {
+			timelineMovDino.stop();
+		}
+	}
+	private void pigiato(KeyEvent evento) {
+		arrivatoSù=false;
+		arrivatoGiù=false;
+		if(evento.getText().equals("w")) {
+			timelineMovDino.play();
 		}
 	}
 	public static void main(String[] args) {
